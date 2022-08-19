6618a9daf13482a423a30a4f69ccec803209c175
surivvgithub <111076850+surivvgithub@users.noreply.github.com>
2022-08-20 04:52:57 +0530

    Add files via upload

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -12,21 +12,34 @@ public class ArrayListDemo {
 		l.add("11");
 		l.add("null");
 		System.out.println("ArrayList are" + l + " ");
-		System.out.println(l + " "); //[A, 25, Su, 11] 
+		System.out.println(l + " "); //[A, 25, Su, 11, null] 
 		
-		l.remove("A"); //[25, Su, 11] 
+		l.remove("A"); //[25, Su, 11, null] 
 		System.out.println(l + " ");
 		l.add(2,"Ma");
 		l.add("Jp");
-		System.out.println(l + " "); //[25, Su, Ma, 11, Jp] 
+		System.out.println(l + " "); //[25, Su, Ma, 11, null, Jp] 
 		
 		l.remove(0);
-		System.out.println(l + " "); //[Su, Ma, 11, Jp] 
+		System.out.println(l + " "); //[Su, Ma, 11, null, Jp] 
 		l.remove(0);
-		System.out.println(l + " "); //[Ma, 11, Jp] 
+		System.out.println(l + " "); //[Ma, 11, null, Jp] 
 		l.add(0,"Su"); 
 		System.out.println(l + " ");  //[Su, Ma, 11, Jp]
 	
 	}
 
 }
+
+
+/* Output
+ 
+ArrayList are[A, 25, Su, 11, null] 
+[A, 25, Su, 11, null] 
+[25, Su, 11, null] 
+[25, Su, Ma, 11, null, Jp] 
+[Su, Ma, 11, null, Jp] 
+[Ma, 11, null, Jp] 
+[Su, Ma, 11, null, Jp] 
+
+*/
